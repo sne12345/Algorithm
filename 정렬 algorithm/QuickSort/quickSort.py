33752dc5162533ad87ed6a43c941d4fff87e1a3a
Naeun Song <sne12345@naver.com>
2021-11-07 08:27:22 +0900

    Create quickSort.py

---
+++ 정렬 algorithm/QuickSort/quickSort.py
@@ -0,0 +1,21 @@
+
+def quickSort(A, p, r):
+    if p < r:
+        q = partition(A, p, r)
+        quickSort(A, p, q-1)
+        quickSort(A, q+1, r)
+
+def partition(A, p, r):
+    x = A[r]
+    i = p - 1
+    for j in range(p, r):
+        if A[j] < x:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+    A[i+1], A[r] = A[r], A[i+1]
+    return i+1
+
+arr = [3,1,4,6,2,7,9,3,0,11]
+quickSort(arr, 0, len(arr)-1)
+print(arr)
+
